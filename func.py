ff7af21bb84f01c3f6c1dc48ec6dd08464e552e6
alemagno10 <alemagno03@gmail.com>
2023-03-06 20:48:37 -0300

    aprimorando codigo

--- func.py
+++ func.py
@@ -1,14 +1,12 @@
 import numpy as np
 def para_one_hot(msg : str):
     posicoes = {letra: indice for indice, letra in enumerate('abcdefghijklmnopqrstuvwxyz ')}
-    return np.array([[1 if posicoes[j] == i else 0 for i in range(27)] for j in msg]).T
+    return np.array([[1 if posicoes[j] == i else 0 for i in range(27)] for j in msg.lower()]).T
 
 def para_string(M):
     M = M.T
-    alfabeto = 'abcdefghijklmnopqrstuvwxyz '
-    resultado = ''
-    row,col = M.shape
-    for i in range(row):
+    alfabeto, resultado = 'abcdefghijklmnopqrstuvwxyz ', ""
+    for i in range(M.shape[0]):
         resultado += alfabeto[np.where(M[i] ==  1)[0][0]]
     return resultado
 
@@ -19,45 +17,30 @@ def de_cifra(msg, M):
     return para_string(np.linalg.inv(M) @ para_one_hot(msg))
 
 def enigma(msg, P, E):
-    final = np.array([0 for i in range(27)])
-    hotMsg = para_one_hot(msg)
-    size = hotMsg.shape
-    for i in range(size[1]):
-        if i > 1:
-            x = E
-            for _ in range(i-1):
-                x = E @ x
-            x = x @ P @ hotMsg
-        elif i == 1:
-            x = E @ P @ hotMsg
-        elif i == 0:
-            x = P @ hotMsg
-
-        if i != 0:
-            final = np.vstack((final,x.T[i]))
-        else:
-            final = x.T[i]
+    hotMsg = para_one_hot(msg).T
+    final = P @ hotMsg[0]
+    for i in range(1,hotMsg.shape[0]):
+        x = P @ hotMsg[i]
+        for _ in range(i):
+            x = E @ x
+        final = np.vstack((final,x.T))
     return para_string(final.T)
 
 def de_enigma(msg, P, E):
     msg = para_one_hot(msg).T
-    size = msg.shape
-    final = np.array([0 for i in range(27)])
-    for i in range(size[0]):
+    final = np.linalg.inv(P) @ msg[0]
+    for i in range(1,msg.shape[0]):
         x = msg[i] 
-        if i == 0: 
-            final = np.linalg.inv(P) @ x
-        else:
-            for _ in range(i):
-                x = np.linalg.inv(E) @ x 
-            final = np.vstack((final, np.linalg.inv(P) @ x))
+        for _ in range(i):
+            x = np.linalg.inv(E) @ x 
+        final = np.vstack((final, np.linalg.inv(P) @ x))
     return para_string(final.T)
 
 alfabeto_cifrado = para_one_hot("bcdefghijkl mnopqrstuvwxyza")
 cifrador_auxiliar = para_one_hot("ijkl mnopqrstuvwxyzabcdefgh")
 mensagem_entrada = "o bolo de chocolate fica pronto quatro horas da tarde"
-mensagem_entrada = "alemagno"
 x = enigma(mensagem_entrada, alfabeto_cifrado, cifrador_auxiliar)
 y = de_enigma(x, alfabeto_cifrado, cifrador_auxiliar)
 print(x,y)
+
 
