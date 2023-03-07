642d912228eaddf6515311eb9c493a1eae925cfd
PedroPertusi <pedrovazpertusi@gmail.com>
2023-03-07 12:17:03 -0300

    teste

--- enigma/setup.py
+++ enigma/setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='enigma',
     version='1.0',
-    packages=find_packages(),
+    packages=['enigma'],
     install_requires=[
         'Numpy',
         # Add other dependencies as needed
     ],
+    url='https://github.com/alemagno10/Enigma.git',
 )
